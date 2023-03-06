98c7871b7f5e1bc2957edc0109e7e6c965a0dfe6
Lunik <ishaandrej@gmail.com>
2023-03-06 16:44:27 +0300

    Evo
    Evo

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/evolution_fog.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_fog-user \
+    evolution_fog-userdebug \
+    evolution_fog-eng
